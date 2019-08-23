d89ca67a2c04f05ecfe1f68ced7666b25e3397e3
Kotawar Akshay <akshaykumarak5g4@gmail.com>
2019-08-23 16:09:10 +0530

    Initial commit
    :wq

---
+++ src/com/servlet/Servlet1.java
@@ -0,0 +1,38 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		String name=request.getParameter("uname");
+		String password=request.getParameter("pwd");
+		
+		request.setAttribute("uname", name);
+		if(password.contentEquals("Isteer"))
+		{
+			RequestDispatcher rd=request.getRequestDispatcher("Servlet2");
+			rd.forward(request, response);
+			
+		}
+		else
+		{
+			
+			RequestDispatcher rd1=request.getRequestDispatcher("index.html");
+			rd1.forward(request, response);
+		}
+	}
+
+}
